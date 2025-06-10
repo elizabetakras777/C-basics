0d21ad283072f0007bb4e9c38466df503b5af4b5
elizabetakras777 <167331161+elizabetakras777@users.noreply.github.com>
2025-06-10 13:21:03 +0200

    Adding my files

--- Test.c
+++ Test.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+// int main() {
+//     printf("Hello, world!\n");
+//     return 0;
+// }
+
 int main() {
     printf("Hello, world!\n");
+  
+
+//student data 
+int studentID = 15;
+int studentAge = 20;
+float studentGPA = 3.5;
+char studentName[] = "John Doe";
+
+//Print variables 
+
+printf("student id: %d\n", studentID);
+printf("student age :%d\n", studentAge);
+printf("student gpa: %f \n", studentGPA);
+printf("student name:%s\n ", studentName);
+
     return 0;
 }
